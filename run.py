ca42578f59e0e0731f781a03709b6dc7e0b4bfbe
ji4 <ji4@users.noreply.github.com>
2024-01-26 05:50:52 +0800

    Write Result to Files

--- run.py
+++ run.py
@@ -6,14 +6,17 @@ from dotenv import dotenv_values
 config = dotenv_values('.env')
 client = OpenAI(api_key = config["API_KEY"])
 
+suffix_bilingual = '_bilingual'
+suffix_translated = '_translated'
+
 def translate_segment(segment, model="gpt-3.5-turbo-1106"):
     try:
-        response = client.chat.completions.create(model=model,
-        messages=[
-            {"role": "system", "content": "You are a professional book translator. Please translate the following English text to Traditional Chinese without adding any extra content or summary."},
-            {"role": "user", "content": f"Translate the following English text to Traditional Chinese:\n\n{segment}"}
-        ],
-        echo=False)
+        response = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "user", 
+                 "content": f"Translate the following English text to Traditional Chinese:\n\n{segment}"}]
+        )
         return response.choices[0].message.content
     except Exception as e:
         print("Error during translation:", e)
@@ -36,6 +39,17 @@ def split_text_smart(text, max_length=600):
 
     return chunks
 
+#Read file
+inputFile = sys.argv[1]
+file_name, file_extension = os.path.splitext(os.path.basename(inputFile))
+f_bilingual = file_name + suffix_bilingual + file_extension
+f_translated = file_name + suffix_translated + file_extension
+
+with open(inputFile, 'r') as f:
+    long_text = f.read()
+    
+folder_path = os.path.dirname(inputFile)
+
 # 分割長文本
 segments = split_text_smart(long_text)
 
@@ -47,14 +61,9 @@ for segment in segments:
         translated_segments.append(translated)
         print(segment+'\n')
         print(translated+'\n')
-
-# 組合翻譯後的文本
-translated_text = "\n".join(translated_segments)inputFile = sys.argv[1]
-#Read file
-inputFile = sys.argv[1]
-with open(inputFile, 'r') as f:
-    long_text = f.read()
-    
-folder_path = os.path.dirname(inputFile)
-
-## 分割長文本
+        with open(folder_path + '/' + f_translated, 'w') as f:
+            f.write(translated)
+            f.write('\n')
+        with open(folder_path + '/' + f_bilingual, 'w') as f:
+            f.write(segment +'\n'+ translated)
+            f.write('\n')
