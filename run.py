c3ef127ff92674df7ac3d4faeb84f1039e6f9f91
ji4 <ji4@users.noreply.github.com>
2024-01-26 11:17:50 +0800

    Add convert_output_filename()

--- run.py
+++ run.py
@@ -38,9 +38,17 @@ def split_text_smart(text, max_length=600):
         chunks.append(current_chunk)
 
     return chunks
+
+def concat_filename_ext(f_name, suffix, f_ext):
+    return f_name + suffix + f_ext
+
+def convert_output_filename(f_name, f_ext):
+    f_bilingual = concat_filename_ext(f_name, suffix_bilingual, f_ext)
+    f_translated = concat_filename_ext(f_name, suffix_bilingual, f_ext)
+    return f_bilingual, f_translated
     
 if __name__ == "__main__":
-    #read file name
+    #read file name, ext, and path
     inputFile = sys.argv[1]
     file_name, file_extension = os.path.splitext(os.path.basename(inputFile))
     folder_path = os.path.dirname(inputFile)
@@ -50,9 +58,7 @@ if __name__ == "__main__":
         long_text = f.read()
 
     #Convert output file name.
-    f_bilingual = file_name + suffix_bilingual + file_extension
-    f_translated = file_name + suffix_translated + file_extension
-
+    f_bilingual, f_translated = convert_output_filename(file_name, file_extension)
 
     # Split long text.
     segments = split_text_smart(long_text)
